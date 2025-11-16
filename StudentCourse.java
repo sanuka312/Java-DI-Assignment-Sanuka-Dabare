d3b97ee54113665422e4135038e694b75a985ca1
Sanuka <sanuka.20232509@iit.ac.lk>
2025-11-16 21:59:16 +0530

    Call the main method in the StudentCourse.java file

---
+++ StudentCourse.java
@@ -0,0 +1,12 @@
+public class StudentCourse {
+    public static void main(String[] args){
+        //creating a course object
+        Course course=new Course();
+
+        //Creating a student object which injects the course object
+        Student student=new Student(course);
+        //calling startLearning() method
+        student.startLearning();
+
+    }
+}
